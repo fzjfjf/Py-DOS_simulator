c1ef9d0eddc924d0f892bb0b53e1b9eb69627820
akadric <abdurrahman.kadric@gmail.com>
2026-02-28 22:23:52 +0100

    Added user support

--- new-emulator.py
+++ new-emulator.py
@@ -3,6 +3,7 @@ from random import randint
 from time import sleep
 from sys import exit, stdin
 from os import name, system
+from hashlib import sha256
 
 # ========= Classes ========
 # ====== Exception classes =====
@@ -26,7 +27,7 @@ class FileSystem:
             drive_label_list.append(randint(0, 9))
         drive_label_list[4] = "-"
         for i in range(0, 9):   # Turn the numbers into strings
-            drive_label_list[i] = str(drive_label_list)
+            drive_label_list[i] = str(drive_label_list[i])
         self._drive_label = "".join(drive_label_list)
 
         # Check fs for correct structure and assign fs to folder_structure
@@ -55,11 +56,40 @@ class FileSystem:
 
         _validate_folder(fs)
 
+        self._folder_structure = fs
+
+    def get_current_folder(self, path_to_use):
+        # Will return a dict of the current folder
+        if path_to_use == "/":
+            return self._folder_structure
+
+        split_path = path_to_use.split("/")
+        current_folder = self._folder_structure
+
+        for part in split_path:
+            if part:
+                current_folder = current_folder["folders"][part]
+
+        return current_folder
+
     def change_directory(self):
         pass
 
-    def dir_command(self):
-        pass
+    def dir_command(self, current_path) -> dict:
+        # Will return a list of all items in the current folder
+
+        current_folder = self.get_current_folder(current_path)
+
+        folders = list(current_folder["folders"].keys())
+        files = current_folder["files"]
+
+        return_value = {
+            "command": "dir",
+            "files": files,
+            "folders": folders,
+            "label": self._drive_label
+        }
+        return return_value
 
     def make_directory(self):
         pass
@@ -67,6 +97,48 @@ class FileSystem:
     def remove_directory_or_file(self):
         pass
 
+
+class UserAccount:
+    def __init__(self):
+        self._user_name = ""
+        self._role = "normal"
+        self._password = ""
+
+    def whoami(self):
+        return self._user_name
+
+    def initialize_user(self, args, role_of_current_user):
+        try:
+            name = args[0]
+            password = args[1]
+            role = args[2]
+        except IndexError:
+            return {"command": "create_user", "exitcode": "invalidsyntax"}
+
+        if role not in ["admin", "normal"]:
+            return {"command": "create_user", "exitcode": "unsupportedrole"}
+
+        if role_of_current_user != "admin" and role == "admin":
+            return {"command": "create_user", "exitcode": "notenoughprivileges"}
+
+        self._user_name = name
+        self._password = sha256(password.encode()).hexdigest()
+        self._role = role
+
+        return {"command": "create_user", "exitcode": "succesful"}
+
+    def get_role(self):
+        return self._role
+
+    def check_password(self, password_to_check) -> bool:
+        if not isinstance(password_to_check, str):
+            return False
+
+        hashed_password = sha256(password_to_check.encode()).hexdigest()
+        if hashed_password == self._password:
+            return True
+
+        return False
 
 
 
@@ -77,15 +149,64 @@ class Kernel:
         self._mounted_drives: dict = {
             "c": FileSystem(),
         }
-        self._kernel_commands: dict = {
-            "mount": self.mount_drive,
-
+        self._current_user: str = "root"
+        self._users = {
+            "admin": UserAccount(),
         }
+        self._users["admin"].initialize_user(["admin", "root", "admin"], "admin")
+        self.shell = Shell(self)
+
+    def on_boot(self):
+        self.mount_drive("c", {"folders": {"docs": {"folders": {}, "files": ["readme.txt", "test.txt"]}},
+                             "files": ["autoexec.bat", "config.sys"]})
+
+        self.login_screen()
+
+    def login_screen(self):
+        # Is shown at the start, or after typing 'logout' into the shell. Basically its own small shell
+        while True:
+            user = input("Enter user name: ")
+            password = input("Enter password: ")
+
+            try:
+                if self._users[user].check_password(password):
+                    self._current_user = user
+                    self.shell.on_start([])
+                    self.shell.command_loop()
+                else:
+                    print("Invalid password")
+            except KeyError:
+                print("Invalid user")
+
+    def create_user(self, args):
+        if args[0] not in self._users:
+            self._users[args[0]] = UserAccount()
+            current_role = self._users[self._current_user].get_role()
+            return_value = self._users[args[0]].initialize_user(args, current_role)
+            if return_value["exitcode"] == "notenoughprivileges":
+                self._users.pop(args[0])
+            return return_value
+
+        return {"command": "createuser", "exitcode": "userexists"}
 
 
     def command_resolver(self, split_user_input):
         # Will resolve non-shell commands like CD, DIR, ...
-        pass
+        try:
+            if split_user_input[0] == "dir":
+                return self._mounted_drives[self._working_drive].dir_command(self._path)
+            elif split_user_input[0] == "cd":
+                new_path = self._mounted_drives[self._working_drive].change_directory(split_user_input[1:])
+                self.update_path(new_path)
+            elif split_user_input[0] == "createuser":
+                return self.create_user(split_user_input[1:])
+            elif split_user_input[0] == "logout":
+                self.shell.clear_screen([])
+                self.login_screen()
+            else:
+                return {"command": "invalid"}
+        except IndexError:
+            return {"command": "createuser", "exitcode": "invalidsyntax"}
 
     def mount_drive(self, drive_letter, fs):
         # Will mount the drive
@@ -105,7 +226,7 @@ class Kernel:
             else:
                 self.bug_check(100, "INVALID_FILE_SYSTEM_STRUCTURE")
 
-    def update_path(self):
+    def update_path(self, new_path):
         pass
 
     def bug_check(self, exit_code, exit_code_for_print):
@@ -123,9 +244,10 @@ class Kernel:
         pass
 
 class Shell:
-    def __init__(self):
+    def __init__(self, kernel):
+        self.kernel = kernel
         self._dos_prompt: str = "C:\\> "
-        self._version_info: str = "PY-DOS 1.1.0 alpha"
+        self._version_info: str = "PY-DOS 1.3.0 alpha"
         self._shell_commands = {
             "ver": self.version,
             "help": self.help,
@@ -141,11 +263,33 @@ class Shell:
         split_input = user_input.lower().split()
         if len(split_input) > 0:        # Ignore empty input
             if split_input[0] == "trigger_bug_check":   # Debug case
-                kernel.bug_check(20, "BUG_CHECK_ON_COMMAND")
+                self.kernel.bug_check(20, "BUG_CHECK_ON_COMMAND")
             elif split_input[0] in self._shell_commands:    # Check if command is owned by Shell
                 self._shell_commands[split_input[0]](split_input[1:])
             else:       # Pass the command to the kernel
-                kernel.command_resolver(split_input)
+                result = self.kernel.command_resolver(split_input)   # Pass the input to the kernel
+
+                if result is not None and isinstance(result, dict):      # If kernel returned something
+                    if result["command"] == "dir":      # If the command was dir
+                        print(f"""Volume in drive C is DOS 
+Volume Serial Number is {result["label"]}
+Directory of {self._dos_prompt.strip("> ")} 
+    
+. <DIR>
+.. <DIR>""")
+                        counter = [2, 0]        # [0] for folders, [1] for files
+                        for folder in result["folders"]:      # Go through all folders, print them with <DIR>, and
+                                                                # increase the counter
+                            print(folder, " <DIR>")
+                            counter[0] += 1
+                        for file in result["files"]:      # Go through all files, print them and increase the counter
+                            print(file)
+                            counter[1] += 1
+                        print(f"Files: {counter[1]}\nFolders: {counter[0]}")    # Print the count
+                    elif result["command"] == "createuser":
+                        print("Invalid syntax") if result["exitcode"] == "invalidsyntax" else None
+                        print("Only root can create new admin users") if result["exitcode"] == "notenoughprivileges" else None
+                        print("User already exists") if result["exitcode"] == "userexists" else None
 
     def command_loop(self):
         while self._is_running:
@@ -216,19 +360,10 @@ Copyright (C) 2026      All rights reserved
 
 
 
-def on_start():
-    global kernel
-    # Replace the placeholder kernel object
-    kernel = Kernel()
-    kernel.mount_drive("c", {"folders": {}, "files": []})
-
-    shell = Shell()
-    return shell
 
 def main():
-    shell = on_start()
-    shell.on_start([])
-    shell.command_loop()
+    kernel = Kernel()
+    kernel.on_boot()
 
 if __name__ == "__main__":
     main()
